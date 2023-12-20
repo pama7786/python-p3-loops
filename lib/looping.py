a6236a0a86a32d50d243e7c937cf24c33e629cfa
abdirahman-abass-diss <abdirahman.diss@student.moringaschool.com>
2023-12-20 07:23:55 +0300

    finished

--- lib/looping.py
+++ lib/looping.py
@@ -11,3 +11,34 @@ def square_integers(int_list):
 def fizzbuzz():
     # code goes here!
     pass
+def happy_new_year():
+    count = 10
+    while count > 0:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
+
+# Test the function
+happy_new_year()
+
+def square_integers(int_list):
+    squared_list = [num ** 2 for num in int_list]
+    return squared_list
+
+# Test the function
+result = square_integers([1, 2, 3, 4, 5])
+print(result)
+
+def fizzbuzz():
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+# Test the function
+fizzbuzz()
